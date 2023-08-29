fb7123f859925a5a1b7691763b95a5a8c4c1df2b
daniel <daniel.alvesdosantos0@gmail.com>
2023-08-28 21:41:35 -0300

    padrao

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const formData = new FormData(form);
  
-    fetch("", {
+    fetch("https://journey-omega.vercel.app/functions/send-email/", {
       method: "POST",
       body: formData,
     })

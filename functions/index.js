8a949724e08076da6730589d9c646f8de51490cd
daniel <daniel.alvesdosantos0@gmail.com>
2023-09-04 17:00:22 -0300

    permitindo acesso do vercel

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,51 @@
-const functions=require("firebase-functions"),admin=require("firebase-admin"),emailjs=require("emailjs-com");admin.initializeApp();const db=admin.firestore();exports.sendEmail=functions.https.onRequest(async(e,s)=>{if("POST"!==e.method)return s.status(405).send("Method Not Allowed");const a=e.body;if(!a.nome||!a.email||!a.mensagem)return s.status(400).send("Campos obrigatórios não preenchidos");try{const e=emailjs;await e.send("service_15ur3yc","template_877lf6q",a,"LSTFzKHBdyB9X8kSN"),await db.collection("mensagens").add({nome:a.nome,email:a.email,mensagem:a.mensagem,timestamp:admin.firestore.FieldValue.serverTimestamp()}),s.status(200).send("E-mail enviado com sucesso")}catch(e){console.error("Erro ao enviar e-mail:",e),s.status(500).send("Erro ao processar a solicitação")}});
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const emailjs = require('emailjs-com');
+
+const express = require('express');
+const app = express();
+
+const corsOrigin = process.env.CORS_ORIGIN || 'https://journey-etecct.web.app';
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', corsOrigin);
+  next();
+});
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.sendEmail = functions.https.onRequest(async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  const formData = req.body;
+
+  if (!formData.nome || !formData.email || !formData.mensagem) {
+    return res.status(400).send('Campos obrigatórios não preenchidos');
+  }
+
+  try {
+    const emailService = emailjs;
+    await emailService.send('service_15ur3yc', 'template_877lf6q', formData, 'LSTFzKHBdyB9X8kSN');
+    
+    await db.collection('mensagens').add({
+      nome: formData.nome,
+      email: formData.email,
+      mensagem: formData.mensagem,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    res.status(200).send('E-mail enviado com sucesso');
+  } catch (error) {
+    console.error('Erro ao enviar e-mail:', error);
+    res.status(500).send('Erro ao processar a solicitação');
+  }
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Servidor está executando na porta 3000');
+});

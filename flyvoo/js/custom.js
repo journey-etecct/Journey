cae1141ceadda7d8cd7927aa9a5d83c23fff190a
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-10-10 00:19:07 -0300

    fazendo a página do produto

---
+++ flyvoo/js/custom.js
@@ -0,0 +1,28 @@
+// to get current year
+function getYear() {
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+document.addEventListener(
+  "scroll",
+  (e) => {
+    console.log(document.documentElement.scrollTop);
+    const scroll = document.documentElement.scrollTop;
+    const header = document.querySelector(".header_section");
+
+    if (scroll > 640) {
+      header.style.background = "#131313";
+    } else {
+      header.style.background = "transparent";
+    }
+  },
+  true
+);
+
+$(".btn1").on("click", () => {
+  window.scroll({ top: 800, behavior: "smooth" });
+});
